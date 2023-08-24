2627279d0b2a70cef62f85cb61ac1ba29b335671
Ian Quinto <codedianq@gmail.com>
2023-08-24 15:06:49 +0800

    Create function called getUserChoice & playRound
    getUserChoice function let the user to type his/her selection.
    playRound function where the user and computer player will compare their selections.

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ The first who will score 5 points will win. Print 'You beat the computer' else '
 
 // Create a variable called numGenerator (generate whole numbers from 1 - 3) for the basis of randomly return 'ROCK, 'PAPER' or 'SCISSORS'
 let numGenerator = Math.floor(Math.random() * 3 + 1)
-console.log(numGenerator);
 
 // Create a function called getComputerChoice that will randomly return either 'ROCK, 'PAPER' or 'SCISSORS'
 function getComputerChoice() {
@@ -26,6 +25,39 @@ function getComputerChoice() {
     }
     return (computerChoice);
 }
+
+//Create a function called getUserChoice. Will get the user choice either 'ROCK, 'PAPER' or 'SCISSORS'
+function getUserChoice() {
+
+    let userChoice = prompt('TYPE AND CHOOSE ONE:', 'ROCK, PAPER, OR SCISSORS?').toUpperCase();
+    return userChoice
+}
+
+
+
+// Create a function called playRound that plays a single round.
+function playRound(userSelection, computerSelection) {
+    if(userSelection === 'ROCK' && computerSelection === 'SCISSORS') {
+        return('You win');
+    } else if (computerSelection === 'ROCK' && userSelection === 'SCISSORS') {
+        return('You lose');
+    } else if(userSelection === 'PAPER' && computerSelection === 'ROCK') {
+        return('You win');
+    } else if (computerSelection === 'PAPER' && userSelection === 'ROCK') {
+        return('You lose');
+    } else if(userSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+        return('You win');
+    } else if (computerSelection === 'SCISSORS' && userSelection === 'PAPER') {
+        return('You lose');
+    } else {
+        return('Tie');
+    }
+}
+
+const userSelection = getUserChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(userSelection, computerSelection));
 
 
 
